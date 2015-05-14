cd5d1ef80ed9e280c624a04a19cb91e875fa075c
Albert Mokrejs <albertmokrejs@gmail.com>
2015-05-14 18:08:25 -0400

    Create VPoint.java

---
+++ VPoint.java
@@ -0,0 +1,26 @@
+public class VPoint{
+  int x;
+  int y;
+  
+  public VPoint(int a, int b){
+    x = a;
+    y = b;
+  }
+  
+  public void setX(int a){
+    x = a;
+  }
+  
+  public void setY(int a){
+    y = a;
+  }
+  
+  public int getX(){
+    return x;
+  }
+  
+  public int getY(){
+    return y;
+  }
+  
+}
