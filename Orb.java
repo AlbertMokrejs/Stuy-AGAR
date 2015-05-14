c900caffcd6c19cd682ff0b788e20b3c0c0c48bd
Albert Mokrejs <albertmokrejs@gmail.com>
2015-05-14 18:18:58 -0400

    Update Orb.java

--- Orb.java
+++ Orb.java
@@ -7,7 +7,7 @@ public class Orb{
   //speed not tracked but calculated when needed
   
   public int getSpeed(){
-    int y = 20 - 2log(getS());
+    int y = 20 - 2*(int)Math.log(getS());
     if(y < 2){
       y = 2;
     }
@@ -15,6 +15,22 @@ public class Orb{
   }
   
   public void turn(Arraylist<Orb> orbs){
+    VPoint[] vect = new VPoint[orbs.size()];
+    for(int x = 0; x < orbs.size(); x++){
+      //NEED TO CHECK IF TOO CLOSE SO CAN KILL
+      vect[x] = process(orbs.get(x));
+    }
+    //takes arraylist of orbs and makes vector to process orbs.
+  }
+  
+  
+  
+  public VPoint process(Orb a){
+    int score = getS() - a.getS(); //negative score negates direction
+    int x = a.getX() - getX(); //saves direction of X and Y
+    int y = a.getY() - getY();
+    score = 20 * score / Math.sqrt(x^2 + y^2); //alters intensity, somewhat arbitrary
+    return new VPoint(score*x, score*y);
   }
   
   public void setX(int a){
