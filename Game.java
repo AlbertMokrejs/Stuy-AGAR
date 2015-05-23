560553df1a16074d5836b6fb41ac12d92af9ef79
Albert Mokrejs <albertmokrejs@gmail.com>
2015-05-23 11:47:06 -0400

    WIP

--- Game.java
+++ Game.java
@@ -1,22 +1,38 @@
 import java.util.*;
 
-public class Game{
-  Arraylist<Orb> orblist;
-  boolean alive;
-  int score;
-  
-  public Game(){
-    //creates game
-  }
-  
-  public void turn(){
-    //spawns a few random orbs
-    //runs through orb array, processing each orb
-    //checks if player survived
-    //incremements score if player alive and size has increased
-  }
-  
-  
-  
-  
+ArrayList<Orb> orblist;
+boolean alive;
+int score;
+//playerOrb player;
+
+public void setup() {
+  size(800,800);
+  alive = true;
+  score = 0;
+  orblist = new ArrayList<Orb>();
+  orblist.add(new Orb(20,20,19));
+  orblist.add(new Orb(-200,-50,10));
+  stroke(255);     // Set line drawing color to white
+  frameRate(60);
+  //player = new PlayerOrb();
 }
+
+public void draw(){
+  background(0);
+  color c = color(153);
+  fill(c);
+   //for(int x = 0; x < 2;){
+     orblist.add(new Orb((int)(Math.random() * 2 * height - height), (int)(Math.random() * 2 * width - width), (int)(Math.random() * 3)));
+   //}   
+  //spawns a few random orbs
+    for(int x = 0; x < orblist.size(); x++){
+      Orb a = orblist.get(x);
+    a.turn(orblist);
+  fill(c);
+    ellipse((float)a.getX(), (float)a.getY(), (float)a.getS()*5, (float)a.getS()*5);
+  }
+  //runs through orb array, processing each orb
+  //checks if player survived
+  //incremements score if player alive and size has increased
+}
+
