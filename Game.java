3d8f6e42768207b8529e20a395acf1b70f7c2110
Albert Mokrejs <albertmokrejs@gmail.com>
2015-05-25 17:37:05 -0400

    No idea why nothing works

--- Game.java
+++ Game.java
@@ -3,18 +3,19 @@ import java.util.*;
 ArrayList<Orb> orblist;
 boolean alive;
 int score;
-//playerOrb player;
+playerOrb player;
 
 public void setup() {
   size(800,800);
   alive = true;
+  player = new playerOrb();
   score = 0;
   orblist = new ArrayList<Orb>();
-  orblist.add(new Orb(20,20,19));
-  orblist.add(new Orb(-200,-50,10));
+  orblist.add(new Orb(400,400,19));
+  orblist.add(player);
   stroke(255);     // Set line drawing color to white
   frameRate(60);
-  //player = new PlayerOrb();
+  
 }
 
 public void draw(){
@@ -22,12 +23,14 @@ public void draw(){
   color c = color(153);
   fill(c);
    //for(int x = 0; x < 2;){
-     orblist.add(new Orb((int)(Math.random() * 2 * height - height), (int)(Math.random() * 2 * width - width), (int)(Math.random() * 3)));
+     orblist.add(new Orb((int)(Math.random()*2*height - height), (int)(Math.random()*2*width - width), (int)(Math.random() * 3)));
    //}   
   //spawns a few random orbs
     for(int x = 0; x < orblist.size(); x++){
       Orb a = orblist.get(x);
-    a.turn(orblist);
+      a.turn(orblist, player);
+    }
+    for(Orb a: orblist){
   fill(c);
     ellipse((float)a.getX(), (float)a.getY(), (float)a.getS()*5, (float)a.getS()*5);
   }
